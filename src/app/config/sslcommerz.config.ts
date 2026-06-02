1b12997407aa3f69290b6a1e67698499b486a9ea
sakibWebDev <sakibeazom2002@gmail.com>
2026-06-02 19:51:47 +0600

    first commsissddt

--- src/app/config/sslcommerz.config.ts
+++
@@ -1,9 +0,0 @@
-// config/sslcommerz.config.ts
-import { SslCommerz } from "@siamf/sslcommerz";
-import { envVars } from "./env";
-
-export const sslcz = new SslCommerz(
-  envVars.SSL_COMMERZ_STORE_ID,
-  envVars.SSL_COMMERZ_STORE_PASSWORD,
-  envVars.SSL_COMMERZ_IS_LIVE === 'true'
-);
